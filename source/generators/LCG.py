e270d4a665f911ab5de6c4a6f187a6950e47de5c
mabuszka <magda@buszka.eu>
2022-11-20 22:44:30 +0100

    Revert "adds GLCG and LCG generators"
    This reverts commit 909c16c47e1d6a7ed8e1f522ccdef174aa6e744f.

--- source/generators/LCG.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-# from collections.abc import Generator
-
-def lcg_gen(M: int, a: int, c: int, seed: int)  # -> Generator[int, None, None]:
-    """LCG - linear congruential generator"""
-    while True:
-        seed = (a * seed + c) % M
-        yield seed
-
