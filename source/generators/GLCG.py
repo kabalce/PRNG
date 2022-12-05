58c6b8d985a07d1d4322cb6614dd9f93f4c67124
mabuszka <magda@buszka.eu>
2022-12-05 09:56:14 +0100

    fix minor bug

--- source/generators/GLCG.py
+++ source/generators/GLCG.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def glcg_gen(M: int, a_seq, seed_seq)  # add types
+def glcg_gen(M: int, a_seq, seed_seq):  # add types
     """LCG - linear congruential generator"""
     while True:
         x = np.sum(a_seq * seed_seq) % M
