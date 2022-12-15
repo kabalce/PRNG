5bedb6ac9ed6b5e04636f8bf862e5c5c694d3365
Klaudia <k.p.balcer@gmail.com>
2022-12-15 10:14:07 +0100

    abstract generator class

---
+++ source/generators/abstract_generator.py
@@ -0,0 +1,32 @@
+from collections.abc import Generator
+import numpy as np
+
+
+class AbstractGenerator(Generator):
+    def __init__(self, m: int = 2 ** 18):
+        self.M = m
+
+    def send(self, value):
+        """
+        Generate the next number and update needed parameters
+        :param value:
+        :return: random integer
+        """
+        return_value = 0
+        return return_value
+
+    def throw(self, typ=None, value=None, traceback=None):
+        raise StopIteration
+
+    def sample(self, length: int = 1):
+        """
+        Sample numbers from iterator
+        :param length: length of sampled sequence
+        :return: np array of shape (length, )
+        """
+        return np.array([next(self) for _ in range(length)])
+
+
+if __name__ == "__main__":
+    generator = AbstractGenerator()
+    print(generator.sample(10))
