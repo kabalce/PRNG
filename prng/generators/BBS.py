70de6fa6cad6207a4569fdab6116b774928e12fa
Klaudia <k.p.balcer@gmail.com>
2022-12-18 09:17:43 +0100

    debug, add freq monobit test and BBs genertor

--- prng/generators/BBS.py
+++ prng/generators/BBS.py
@@ -3,8 +3,8 @@ from datetime import datetime
 from typing import Optional
 
 
-class LinearCongruentialGenerator(AbstractGenerator):
-    def __init__(self, m: int = 2 ** 18, a: Optional[int] = None, c: Optional[int] = None, seed: Optional[int] = None):
+class BlumBlumShubGenerator(AbstractGenerator):
+    def __init__(self, m: int = 993319 * 23, seed: Optional[int] = None):
         """
 
         :param m: modulus
@@ -14,15 +14,13 @@ class LinearCongruentialGenerator(AbstractGenerator):
         """
         now = datetime.now()
         super().__init__(m)
-        self.a = a if a is not None else now.minute % self.M
-        self.c = c if c is not None else now.second % self.M
         self.seed = seed if seed is not None else now.microsecond % self.M
 
     def send(self, ignored_arg: None = None) -> int:
-        self.seed = (self.a * self.seed + self.c) % self.M
-        return self.seed
+        self.seed = (self.seed ** 2) % self.M
+        return int(bin(self.seed)[-9:], 2)
 
 
 if __name__ == "__main__":
-    generator = LinearCongruentialGenerator()
+    generator = BlumBlumShubGenerator()
     print(generator.sample(10))
--- prng/tests.py
+++ prng/tests.py
@@ -3,9 +3,10 @@ import numpy.typing as npt
 import scipy.stats as stats
 from typing import Tuple, Optional
 from abc import ABC
-from scipy.special import erfc
-from bitarray import bitarray
-
+from scipy.special import erfc, binom
+# from math import factorial
+# from bitarray import bitarray
+# from decimal import Decimal
 
 class Tester(ABC):
     def __init__(self, data: npt.NDArray[int], m: Optional[int] = None, no_folds: int = 100):
@@ -15,6 +16,8 @@ class Tester(ABC):
         self.binaries = None
         self.n = self.integers.shape[0]
         self.fold_index = [(self.n * i) // no_folds for i in range(no_folds + 1)]
+        self.no_folds = no_folds
+        self.no_folds_bin = None
 
     def _provide_uniform(self):
         if self.uniform is None:
@@ -23,10 +26,10 @@ class Tester(ABC):
     def _provide_binary(self):
         if self.binaries is None:
             byt = bytearray(self.integers)
-            ba = bitarray()
+            ba = ''
             for b in bytes(byt):
-                ba += bitarray(b)
-            self.binaries = ba
+                ba += bin(b)[2:]
+            self.binaries = np.array([bool(int(i)) for i in ba])
 
     @staticmethod
     def _chi2_stat(uniform: npt.NDArray[float], k: int = 1000) -> float:
@@ -48,7 +51,7 @@ class Tester(ABC):
 
     def ch2_2nd_level_test(self, k: int = 1000, alpha: float = 0.05) -> Tuple[float, float, bool]:
         self._provide_uniform()
-        p_values = np.array([self._chi2_pval(k, self._chi2_stat(self.uniform[self.fold_index[i]: self.fold_index[i + 1]], k)) for i in range(len(self.fold_index) - 1)])
+        p_values = np.array([self._chi2_pval(k, self._chi2_stat(self.uniform[self.fold_index[i]: self.fold_index[i + 1]], k)) for i in range(self.no_folds)])
         t = self._chi2_stat(p_values)
         p = self._chi2_pval(k, t)
         return p, t, p < alpha
@@ -76,7 +79,7 @@ class Tester(ABC):
 
     def bds_2nd_level_test(self, k: int = 10, k_loc: int = 512, alpha: float = 0.05) -> Tuple[float, float, bool]:
         l = int(self.n ** 3 / (4 * k_loc))
-        poi_stats = np.array([self._bds_stat(self.integers[self.fold_index[i]: self.fold_index[i + 1]], k_loc) for i in range(len(self.fold_index) - 1)])
+        poi_stats = np.array([self._bds_stat(self.integers[self.fold_index[i]: self.fold_index[i + 1]], k_loc) for i in range(self.no_folds)])
 
         distr = stats.poisson(l)
         quantiles = np.flip(distr.ppf(np.arange(k) / k).astype(int))
@@ -95,19 +98,19 @@ class Tester(ABC):
 
     def ks_2nd_level_test(self, alpha: float = 0.05) -> Tuple[float, float, bool]:
         self._provide_uniform()
-        p_values = np.array([stats.kstest(self.uniform[self.fold_index[i]: self.fold_index[i + 1]], stats.uniform().cdf).pvalue for i in range(len(self.fold_index) - 1)])
+        p_values = np.array([stats.kstest(self.uniform[self.fold_index[i]: self.fold_index[i + 1]], stats.uniform().cdf).pvalue for i in range(self.no_folds)])
         res = stats.kstest(p_values, stats.uniform().cdf)
         return res.pvalue, res.statistic, res.pvalue < alpha
 
     @staticmethod
-    def _runs_pre_test(binaries: bitarray) -> Tuple[bool, float]:
-        pi = len(binaries.search(bitarray('1'))) / len(binaries)
-        tau = 2 / np.sqrt(len(binaries))
+    def _runs_pre_test(binaries: npt.NDArray[bool]) -> Tuple[bool, float]:
+        pi = binaries.sum() / binaries.shape[0]
+        tau = 2 / np.sqrt(binaries.shape[0])
         return pi >= tau, pi
 
     @staticmethod
-    def _runs_stat(binaries: bitarray) -> float:
-        stat = len(binaries.search(bitarray('01'))) + len(binaries.search(bitarray('10'))) + 2
+    def _runs_stat(binaries: npt.NDArray[bool]) -> float:
+        stat = (binaries[1: ] != binaries[: -1]).sum() + 2
         return stat
 
     @staticmethod
@@ -125,16 +128,62 @@ class Tester(ABC):
         else:
             return None, None, False
 
+    def runs_2nd_level_test(self, k: int = 10, alpha: float = 0.05) -> Tuple[Optional[float], Optional[float], bool]:
+        self._provide_binary()
+        n = 512
+        self.no_folds_bin = self.binaries.shape[0] // n
+        folds_bin = [n * i for i in range(self.no_folds_bin + 1)]
+
+        p_values = np.array([self.runs_test(self.binaries[folds_bin[i]: folds_bin[i + 1]])[0] for i in range(self.no_folds_bin)])
+        nans = np.isnan(p_values).sum()
+        p_values = p_values[~np.isnan(p_values)]
+
+        prob_none = np.array([binom(n, i) for i in range(int(np.ceil(n / 2 + 2 * np.sqrt(n))))]).sum() / (2 ** n)
+
+        y = np.flip(np.bincount((p_values.reshape(-1, 1) < (np.arange(k) / k)[np.newaxis, :]).sum(axis=1), minlength=k))
+        t = (np.square(y - self.no_folds_bin / k * (1 - prob_none)) / (self.no_folds_bin / k * (1 - prob_none))).sum() + (nans - self.no_folds_bin * prob_none) ** 2 / (n * prob_none)
+        p = 1 - stats.chi2(k).cdf(t)
+        return p, t, p < alpha
+
+    @staticmethod
+    def _freq_monobit_stat(binaries: npt.NDArray[bool]) -> float():
+        t = np.abs((binaries * 2 - 1).sum()) / np.sqrt(binaries.shape[0])
+        return t
+
+    @staticmethod
+    def _freq_monobit_pval(t: float) -> float:
+        p = erfc(t / np.sqrt(2))
+        return p
+
+    def freq_monobit_test(self, alpha: float = 0.05) -> Tuple[Optional[float], Optional[float], bool]:
+        self._provide_binary()
+        t = self._freq_monobit_stat(self.binaries)
+        p = self._freq_monobit_pval(t)
+        return p, t, p < alpha
+
+    def freq_monobit_2nd_level_test(self, k: int = 1000, alpha: float = 0.05) -> Tuple[Optional[float], Optional[float], bool]:
+        n = 512
+        self.no_folds_bin = self.binaries.shape[0] // n
+        folds_bin = [n * i for i in range(self.no_folds_bin + 1)]
+        p_values = np.array([self._freq_monobit_pval(self._freq_monobit_stat(self.binaries[folds_bin[i]: folds_bin[i + 1]])) for i in range(self.no_folds_bin)])
+        t = self._chi2_stat(p_values)
+        p = self._chi2_pval(k, t)
+        return p, t, p < alpha
+
 
 if __name__ == "__main__":
     from prng.generators.LCG import LinearCongruentialGenerator as LCG
     generator = LCG()
-    data = generator.sample(2 ** 15)
+    data = generator.sample(2**15)
     tester = Tester(data)
-    print(tester.runs_test())
+
+    print(tester.freq_monobit_test())
+    print(tester.freq_monobit_2nd_level_test())
     print(tester.chi2_test())
     print(tester.ch2_2nd_level_test())
     print(tester.bds_test())
     print(tester.bds_2nd_level_test())
     print(tester.ks_test())
     print(tester.ks_2nd_level_test())
+    print(tester.runs_test())
+    print(tester.runs_2nd_level_test())
